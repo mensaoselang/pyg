e581c8560f17b57b51f94211856cd01d3ef09092
FJMKING <1220390544@qq.com>
2018-11-06 22:06:44 +0800

    优化加载和添加本地存储

--- js/classification.js
+++ js/classification.js
@@ -1,10 +1,51 @@
 $(function(){
+    //定义左侧滚动条对象
+    var LeftScroll;
     init();
 var Categories;
     //初始化
     function init(){
         setHtmlFont();
-        getList();
+        // getList();
+        renderMain();
+        eventList();
+    }
+    //数据渲染
+    function renderMain(){
+     //获取本地存储中的分类数据
+     var localDataStr=sessionStorage.getItem("cates");
+     //判断是否存在本地数据
+     if(!localDataStr){
+         //发送请求获取数据
+         getList();
+     }else{
+        var localData=JSON.parse(localDataStr);//将json字符串解析成对象
+        //判断数据是否过期
+        if(Date.now()-localData.time>360000){
+            //过期重新请求数据
+            getList();
+        }else{
+         Categories=localData.data;
+         getLeftmenu();
+         getRightmenu(0);
+        }
+     }
+
+    }
+    //注册事件
+    function eventList(){
+        //委托注册左侧点击事件
+        $(".left_menu").on("tap","li",function(){
+        //   console.log(this);
+        //点击激活
+        $(this).addClass("active").siblings().removeClass("active");
+        //获得当前点击索引
+        var index=$(this).index();
+        //右侧跟随改变
+        getRightmenu(index);
+        //点击置顶
+        LeftScroll.scrollToElement(this);
+        })
     }
     //获取商品列表数据
     function getList(){
@@ -13,12 +54,18 @@ var Categories;
             if(res.meta.status==200){
               //   console.log(res);
                 Categories=res.data;
-              
+              //将数据存入本地存储
+              sessionStorage.setItem("cates",JSON.stringify({
+                  time:Date.now(),
+                  data:Categories
+              }));
               //  console.log(Categories[0].children);
               //   var htmlStr=template("rightTemplate",{data:Categories[0].children});
               //   $(".right_menu").html(htmlStr);
               getLeftmenu();
               getRightmenu(0);
+            }else{
+                console.log("failed");
             }
           })
     }
@@ -27,19 +74,32 @@ var Categories;
          //准备字符串模板
          var html=template("leftTemplate",{data:Categories});
          //渲染数据到页面上
-         $(".left_menu ul").html(html);
+         $(".menu_wrap ").html(html).hide().fadeIn(3000);
+         //初始化左侧滚动条
+         LeftScroll=new IScroll(".left_menu");
     }
   //获取右边数据
    function getRightmenu(index){
     var htmlStr=template("rightTemplate",{data:Categories[index].children});
-    $(".right_menu").html(htmlStr);
+    $(".right_menu").html(htmlStr).hide().fadeIn(3000);
+ 
+
+    var nums=$(".right_menu img").length;//获取当前要加载的图片的数量
+    // console.log(nums);
+    $(".right_menu img").on("load",function(){
+        nums--;//当最后一张图片加载完毕后
+        if(nums==0){
+               //初始化右侧滚动条
+            var rightscroll=new IScroll(".right_menu");
+        }
+    })
    }
    //屏幕适配
    function setHtmlFont(){
        //屏幕基础值
        var baseVal=100;
        //设计稿宽度
-       var pageWidth=640;
+       var pageWidth=320;
        //当前的屏幕宽度
        var screenWidth=document.querySelector("html").offsetWidth;
        //字体大小
@@ -48,7 +108,7 @@ var Categories;
        document.querySelector("html").style.fontSize=fs+'px';
    }
    //屏幕改变重新触发屏幕适配
-   window.onreset=function(){
+   window.onresize=function(){
     setHtmlFont();
    }
 })
--- js/common.js
+++ js/common.js
@@ -2,8 +2,18 @@ $(function(){
     //公共路劲
     var baseUrl="http://api.pyg.ak48.xyz/api/public/v1/";
     //设置拦截器
+    //ajax发送前调用
     $.ajaxSettings.beforeSend = function (xhr, obj) {
         console.log(xhr);
         obj.url = baseUrl  + obj.url;
+        //显示正在等待 
+       $("body").addClass("waiting");
       }
+      //ajax发送结束后调用
+
+    $.ajaxSettings.complete=function(){
+       
+        $("body").removeClass("waiting");
+     }
+
 })
